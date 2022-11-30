8e1ab0e4534c113fef09042fcfdaf5342c976011
CcccFz <ccccfz@163.com>
2022-11-30 11:36:35 +0800

    feat: inherit

---
+++ js-garden/inherit.js
@@ -0,0 +1,20 @@
+function Person() {}
+Person.prototype = {
+    constructor: Person,
+    eat() {console.log('eat');},
+    me() {console.log(this);}
+}
+
+function Child() {}
+
+// 法一：
+Child.prototype = new Person()
+
+// 法二：
+Child.prototype = Object.create(Person.prototype)
+
+
+
+Child.prototype.constructor = Child
+new Child().eat()
+new Child().me()
